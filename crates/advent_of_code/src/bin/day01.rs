9a41e7ad587cf73d2eb0bee48c988f40aebf7d58
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2025-11-15 20:09:16 +0530

    add solution of day01 of advent of code

---
+++ crates/advent_of_code/src/bin/day01.rs
@@ -0,0 +1,46 @@
+use std::{env, fs, iter};
+
+fn read_data_filename() -> String {
+    let args = env::args();
+    if args.len() < 2 {
+        panic!("Please specify data filepath.");
+    }
+
+    match args.last() {
+        Some(filename) => filename,
+        None => panic!("Please specify data filepath."),
+    }
+}
+
+fn main() {
+    let mut left_vec: Vec<u32> = vec![];
+    let mut right_vec: Vec<u32> = vec![];
+
+    // Read file and split lines
+    for val in fs::read_to_string(read_data_filename())
+        .expect("read data file")
+        .lines()
+    {
+        // parse to numbers
+        let mut v = val.split_whitespace();
+
+        let left_num = v.next().unwrap().parse::<u32>().unwrap();
+        left_vec.push(left_num);
+
+        let right_num = v.next().unwrap().parse::<u32>().unwrap();
+        right_vec.push(right_num);
+    }
+
+    left_vec.sort();
+    right_vec.sort();
+
+    let difference_sum: u32 = iter::zip(left_vec, right_vec)
+        .map(|(l, r)| {
+            // Calculate difference of corresponding numbers
+            // sum the difference > result
+            l.abs_diff(r)
+        })
+        .sum();
+
+    println!("Result of day1: {}", difference_sum);
+}

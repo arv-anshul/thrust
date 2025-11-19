c6185adeda4315eb068f66fca80d736d21934a01
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2025-11-19 21:05:39 +0530

    implement robust point system

---
+++ src/bin/point.rs
@@ -0,0 +1,238 @@
+use std::fmt;
+use std::ops::{Add, Div, Mul, Neg, Sub};
+
+trait Coordinate:
+    fmt::Display
+    + Copy
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + Neg<Output = Self>
+    + PartialEq
+{
+    fn zero() -> Self;
+    fn powi(self, n: i32) -> Self;
+    fn sqrt(self) -> Self;
+    fn abs(self) -> Self;
+}
+
+impl Coordinate for f32 {
+    fn zero() -> Self {
+        0.0_f32
+    }
+
+    fn powi(self, n: i32) -> Self {
+        f32::powi(self, n)
+    }
+
+    fn sqrt(self) -> Self {
+        f32::sqrt(self)
+    }
+
+    fn abs(self) -> Self {
+        f32::abs(self)
+    }
+}
+
+impl Coordinate for f64 {
+    fn zero() -> Self {
+        0.0_f64
+    }
+
+    fn powi(self, n: i32) -> Self {
+        f64::powi(self, n)
+    }
+
+    fn sqrt(self) -> Self {
+        f64::sqrt(self)
+    }
+
+    fn abs(self) -> Self {
+        f64::abs(self)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+struct Point<T: Coordinate> {
+    x: T,
+    y: T,
+}
+
+impl<T: Coordinate> fmt::Display for Point<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl Into<Point<f64>> for Point<f32> {
+    /// Converts Point<f32> to Point<f64>
+    fn into(self) -> Point<f64> {
+        Point {
+            // Use the into() method from the f32 implementation above
+            x: self.x.into(),
+            y: self.y.into(),
+        }
+    }
+}
+
+impl<T: Coordinate> Point<T> {
+    pub fn new(x: T, y: T) -> Point<T> {
+        Point { x, y }
+    }
+
+    pub fn origin() -> Point<T> {
+        Point {
+            x: T::zero(),
+            y: T::zero(),
+        }
+    }
+
+    pub fn euclidean_distance(&self, other: &Point<T>) -> T {
+        let dx = other.x - self.x;
+        let dy = other.y - self.y;
+        (dx.powi(2) + dy.powi(2)).sqrt()
+    }
+
+    pub fn is_collinear(&self, p2: &Point<T>, p3: &Point<T>) -> bool {
+        let x = self.x * (p2.y - p3.y);
+        let y = p2.x * (self.y - p3.y);
+        let z = p3.x * (p2.y - self.y);
+        (x + y + z) == T::zero()
+    }
+
+    pub fn to_quad1(self) -> Self {
+        Self {
+            x: self.x.abs(),
+            y: self.y.abs(),
+        }
+    }
+
+    pub fn to_quad2(self) -> Self {
+        Self {
+            x: self.x.abs(),
+            y: -self.y.abs(),
+        }
+    }
+
+    pub fn to_quad3(self) -> Self {
+        Self {
+            x: -self.x.abs(),
+            y: -self.y.abs(),
+        }
+    }
+
+    pub fn to_quad4(self) -> Self {
+        Self {
+            x: -self.x.abs(),
+            y: self.y.abs(),
+        }
+    }
+}
+
+impl<T: Coordinate> Add for Point<T> {
+    type Output = Point<T>;
+
+    fn add(self, other: Point<T>) -> Point<T> {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl<T: Coordinate> Sub for Point<T> {
+    type Output = Point<T>;
+
+    fn sub(self, other: Point<T>) -> Point<T> {
+        Point {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+impl<T: Coordinate> Mul for Point<T> {
+    type Output = Point<T>;
+
+    fn mul(self, other: Point<T>) -> Point<T> {
+        Point {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        }
+    }
+}
+
+impl<T: Coordinate> Div for Point<T> {
+    type Output = Point<T>;
+
+    fn div(self, other: Point<T>) -> Point<T> {
+        Point {
+            x: self.x / other.x,
+            y: self.y / other.y,
+        }
+    }
+}
+
+fn main() {
+    // Example of using the Point struct
+    let p1 = Point {
+        x: 3.5f32,
+        y: 4.2f32,
+    };
+    let p2 = Point::new(23.0, 1.1);
+
+    println!("Origin Point: {}", Point::<f64>::origin());
+    println!("Point 1: {}", p1);
+    println!("Point 2: {}", p2);
+
+    // Arithmetic operations on Points
+    println!();
+    println!("> Add: {} + {} = {}", p1, p2, p1.clone() + p2.clone());
+    println!("> Sub: {} - {} = {}", p1, p2, p1.clone() - p2.clone());
+    println!("> Mul: {} * {} = {}", p1, p2, p1.clone() * p2.clone());
+    println!("> Div: {} ÷ {} = {}", p1, p2, p1.clone() / p2.clone());
+
+    // Calculate euclidean distance
+    println!();
+    println!(
+        "> Euclidean Distance between {} and {} is {}",
+        p2,
+        p1,
+        p2.euclidean_distance(&p1),
+    );
+
+    // Move to different Quadrants
+    println!();
+    let p3 = Point::new(-1.8, -4.9);
+    println!(
+        "> Move point {} to 1st quadrant: {}",
+        p3,
+        p3.clone().to_quad1()
+    );
+    println!(
+        "> Move point {} to 2nd quadrant: {}",
+        p3,
+        p3.clone().to_quad2()
+    );
+    println!(
+        "> Move point {} to 3rd quadrant: {}",
+        p3,
+        p3.clone().to_quad3()
+    );
+    println!(
+        "> Move point {} to 4th quadrant: {}",
+        p3,
+        p3.clone().to_quad4()
+    );
+
+    // Check collinear
+    println!();
+    println!(
+        "> Are collinear {} - {} - {}?: {}",
+        p1,
+        p2,
+        p3,
+        p1.is_collinear(&p2, &p3)
+    );
+}
